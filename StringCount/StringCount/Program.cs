30acd590c2bc0edbdd230ed4d375aefaa11701b0
faskaa <fazilnb@code.edu.az>
2023-08-28 16:33:56 +0400

    first commit

---
+++ StringCount/StringCount/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StringCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello, this is my count task. Good days!";
+            int count = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i]==' ')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+
+
+
+
+
+
+        }
+    }
+}
